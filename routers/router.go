71c827cfb8022f6f503cb5bc20fd1290cca4f0cc
saidurcsesust <saidur.cse.sust19@gmail.com>
2026-01-22 10:49:47 +0600

    project setup and initial structure

---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"property_listing_api/controllers"
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
